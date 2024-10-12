39fa3c8b63848ca37d6e671b7ce3890b1c07f946
NuwanJ <nuwanjaliyagoda@gmail.com>
2024-10-12 11:57:16 +0530

    C3 basic tests

--- src/define.h
+++ src/define.h
@@ -2,7 +2,6 @@
 
 #include <Arduino.h>
 
-#include <Servo.h>
 #include <BluetoothSerial.h>
 
 // Load the configuration details
--- src/src.cpp
+++ src/src.cpp
@@ -1,72 +1,45 @@
-
 #include <Arduino.h>
 #include "define.h"
 
-BluetoothSerial SerialBT;
-Servo servoSpeed;
-Servo servoSteering;
+#include <ESP32Servo.h>
 
-int speed = 90;    // Default position for Servo 1
-int steering = 90; // Default position for Servo 2
+Servo myServo; // create servo object to control a servo
 
-// This is to hide non-test related source code.
-// https://docs.platformio.org/en/latest/plus/unit-testing.html
-#ifndef UNIT_TEST
+// Possible PWM GPIO pins on the ESP32-C3:
+// 0(used by on-board button),1-7,8(used by on-board LED),9-10,18-21
+
+int servoPin = 2;
+int potPin = 4;
+
+int ADC_Max = 4096;
+
+int val;
 
 void setup()
 {
-    // put your setup code here, to run once:
+    delay(2000);
 
-    // Enables Serial Communication with baudRate of 115200
     Serial.begin(115200);
-
     pinMode(PIN_LED_INBUILT, OUTPUT);
 
-    Serial.println("PlatformIO ESP32 Boilerplate started...");
-    SerialBT.begin("ESP32C3_ServoControl"); // Bluetooth name
+    digitalWrite(PIN_LED_INBUILT, HIGH);
+    delay(1000);
+    digitalWrite(PIN_LED_INBUILT, LOW);
+    delay(1000);
 
-    // Attach servos to pins
-    servoSpeed.attach(SERVO_PIN_MOVE);
-    servoSteering.attach(SERVO_PIN_TURN);
-
-    // Set initial position
-    servoSpeed.write(speed);
-    servoSteering.write(steering);
-
-    Serial.println("Bluetooth Servo Control Ready. Waiting for commands...");
+    // Allow allocation of all timers
+    ESP32PWM::allocateTimer(0);
+    ESP32PWM::allocateTimer(1);
+    ESP32PWM::allocateTimer(2);
+    ESP32PWM::allocateTimer(3);
+    myServo.setPeriodHertz(50);
+    myServo.attach(servoPin, 500, 2400);
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
-
-    // Check if data is received over Bluetooth
-    if (SerialBT.available())
-    {
-        digitalWrite(PIN_LED_INBUILT, HIGH);
-        delay(1000);
-        digitalWrite(PIN_LED_INBUILT, LOW);
-        delay(1000);
-
-        String data = SerialBT.readStringUntil('\n'); // Read data until newline
-
-        // Parse the received data to control both servos
-        if (data.startsWith("S1:"))
-        {
-            speed = data.substring(3).toInt(); // Extract angle for Servo 1
-            speed = constrain(speed, 0, 180);  // Ensure it's within 0-180 degrees
-            servoSpeed.write(speed);           // Move Servo 1 to the specified angle
-            Serial.println("Servo 1 Angle: " + String(speed));
-        }
-
-        if (data.startsWith("S2:"))
-        {
-            steering = data.substring(3).toInt();   // Extract angle for Servo 2
-            steering = constrain(steering, 0, 180); // Ensure it's within 0-180 degrees
-            servoSteering.write(steering);          // Move Servo 2 to the specified angle
-            Serial.println("Servo 2 Angle: " + String(steering));
-        }
-    }
+    val = analogRead(potPin);
+    val = map(val, 0, ADC_Max, 0, 180);
+    myServo.write(val);
+    delay(200);
 }
-
-#endif
